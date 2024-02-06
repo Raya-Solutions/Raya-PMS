f18cec19f2f6d81af868519bb75153a650b72230
CliffordArnejo <151482267+CliffordArnejo@users.noreply.github.com>
2024-02-06 15:19:10 +0800

    oneoasis

---
+++ config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "One Oasis",
+			"type": "module",
+			"label": _("One Oasis")
+		}
+	]
---
+++ one_oasis/doctype/paymongo_logs/test_paymongo_logs.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Raya Solutions and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPaymongoLogs(FrappeTestCase):
+	pass
